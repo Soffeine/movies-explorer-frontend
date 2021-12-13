3a90ba158f98470223320a37775f8c2f65418ecc
Soffeine <Soffein@yandex.ru>
2021-12-14 02:19:59 +0300

    changed links

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -14,13 +14,13 @@ function Portfolio() {
                 </li>
                 <li className="portfolio__link-item">
                 <p className="portfolio__link-text">Адаптивный сайт</p>
-                <a href="https://soffeine.github.io/russian-travel/" target="_blank" className="portfolio__link">
+                <a href="https://github.com/Soffeine/russian-travel" target="_blank" className="portfolio__link">
                     <img src={sign} alt="перейти по ссылке" className="portfolio__link-sign" />
                 </a>
                 </li>
                 <li className="portfolio__link-item">
                 <p className="portfolio__link-text">Одностраничное приложение</p>
-                <a href="https://mesto.soffeine.nomoredomains.xyz/" target="_blank" className="portfolio__link">
+                <a href="https://github.com/Soffeine/react-mesto-api-full" target="_blank" className="portfolio__link">
                     <img src={sign} alt="перейти по ссылке" className="portfolio__link-sign" />
                 </a>
                 </li>
