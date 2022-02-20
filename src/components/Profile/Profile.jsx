17436fc93e50b56fec0f0a4e0f028e2171e4fdec
Soffeine <Soffein@yandex.ru>
2022-02-20 13:15:28 +0300

    profile changing fixed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,51 +5,57 @@ import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import { useContext, useState, useEffect } from 'react';
 import { omit } from 'lodash';
 import validator from 'validator';
+import { useRef } from 'react';
 
 
 function Profile({ onSignout, loggedIn, onEditProfile, submitSpan }) {
     const currentUser = useContext(CurrentUserContext);
-    const [profileValues, setProfileValues] = useState({});
-    const [isValid, setIsValid] = useState({});
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('')
     const [errors, setErrors] = useState({});
 
-    useEffect(() => {
-        setProfileValues({
-            name: currentUser.name,
-            email: currentUser.email
-        })
-    },[currentUser])
+    const emailRef = useRef();
+    const formRef = useRef();
 
-    const handleChangeProfile = (e) => {
-        e.persist();
-        let name = e.target.name;
-        let value = e.target.value;
-        validateInput(e, name, value);
-        setProfileValues({
-            ...profileValues,
-            [name]: value,
-        });
+    const [isNeedButtonDisabled, setIsNeedButtonDisabled] = useState(false);
+
+    useEffect(() => {
+        if (currentUser) {
+            setName(currentUser.name);
+            setEmail(currentUser.email);
+        }
+    }, [currentUser])
+
+    const handleChangeName = (e) => {
+        setName(e.target.value);
+        validateInput('name')
     }
 
-    const validateInput = (e, name, value) => {
-        switch(name) {
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+        validateInput('email');
+    }
+
+
+    const validateInput = (inputName) => {
+        switch (inputName) {
             case 'name':
-                if(
+                if (
                     new RegExp
-                    (/[^a-zа-яё\-\s]/gi)
-                    .test(value)
-                    ) {
-                        setErrors({
-                            ...errors,
-                            name: 'Имя может содержать латиницу, кириллицу, дефисы и пробелы'
-                        })
-                    } else {
-                        let newObj = omit(errors, "name");
-                        setErrors(newObj);
-                    }
-                    break;
+                        (/[^a-zа-яё\-\s]/gi)
+                        .test(name || '')
+                ) {
+                    setErrors({
+                        ...errors,
+                        name: 'Имя может содержать латиницу, кириллицу, дефисы и пробелы'
+                    })
+                } else {
+                    let newObj = omit(errors, "name");
+                    setErrors(newObj);
+                }
+                break;
             case 'email':
-                if(!validator.isEmail(value)) {
+                if (!validator.isEmail(emailRef.current.value)) {
                     setErrors({
                         ...errors,
                         email: 'Некорректный адрес'
@@ -59,35 +65,60 @@ function Profile({ onSignout, loggedIn, onEditProfile, submitSpan }) {
                     setErrors(newObj);
                 }
                 break;
-            default: 
+            default:
                 break;
         }
-        Object.keys(errors).length === 0
-        && e.target.closest("form").checkValidity() ? setIsValid(true) : setIsValid(false);
     }
 
     const handleSubmitOnEdit = (e) => {
         e.preventDefault();
-        onEditProfile(profileValues)
+        onEditProfile(name, email)
     }
+
+    const checkNameChange = () => {
+        if (currentUser.name === name) {
+            console.log(true)
+            return true;
+        }
+        console.log(false);
+        return false;
+    }
+
+    const checkEmailChange = () => {
+        if(currentUser.email === email) {
+            console.log(true)
+            return true;
+        }
+        console.log(false);
+        return false;
+    }
+
+    useEffect(() => {
+        setIsNeedButtonDisabled(() => checkNameChange())
+    }, [name])
+
+    useEffect(() => {
+        setIsNeedButtonDisabled(() => checkEmailChange())
+    }, [email])
+
     return (
         <>
             <Header loggedIn={loggedIn} />
             <section className="profile">
                 <UserWelcomeMessage text={`Привет, ${currentUser.name}`} />
-                <form className="profile__form" onSubmit={handleSubmitOnEdit}>
+                <form ref={formRef} className="profile__form" onSubmit={handleSubmitOnEdit}>
                     <div className="profile__input-container">
                         <label className="profile__label">Имя</label>
-                        <input className="profile__input" type="text" name="name" required value={profileValues.name} onChange={handleChangeProfile} />
+                        <input className="profile__input" type="text" name="name" required value={name} onChange={handleChangeName} />
                     </div>
                     <div className="profile__input-error"><p className="profile__input-error-message">{errors.name}</p></div>
                     <div className="profile__input-container">
                         <label className="profile__label">E-mail</label>
-                        <input className="profile__input" type="email" name="email" required value={profileValues.email} onChange={handleChangeProfile} />
+                        <input ref={emailRef} className="profile__input" type="email" name="email" required value={email}  onChange={handleChangeEmail} />
                     </div>
                     <div className="profile__input-error"><p className="profile__input-error-message">{errors.email || submitSpan}</p></div>
                     <div className="profile__button-container">
-                        <button className="profile__button profile__button_submit" type="submit" disabled={!isValid}>Редактировать</button>
+                        <button className="profile__button profile__button_submit" type="submit" disabled={isNeedButtonDisabled}>Редактировать</button>
                         <button className="profile__button profile__button_signout" type="button" onClick={onSignout}>Выйти из аккаунта</button>
                     </div>
                 </form>
